7b58e3b1aedeaf134d677da529966c6727b5ff93
Lachlan Brewster <lbr63@uclive.ac.nz>
2017-08-21 15:15:12 +1200

    Created input handler and created main classes for now

---
+++ src/main/java/seng202/team7/InputHandler.java
@@ -0,0 +1,32 @@
+package seng202.team7;
+
+public class InputHandler {
+
+    public ArrayList<Data> loadCSV(String file)
+    {
+
+    }
+
+    private Boolean checkValidity(Retailer)
+    {
+
+    }
+
+    private Boolean checkValidity(Trip)
+    {
+
+    }
+
+    private Boolean checkValidity(Wifi)
+    {
+
+    }
+
+    private Boolean checkValidity(Station)
+    {
+
+    }
+
+
+
+}
