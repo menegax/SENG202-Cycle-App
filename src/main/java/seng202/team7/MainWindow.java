c9bf1c3f372f92e1d08912e203e724608168dcd4
Connor <cmc274@uclive.ac.nz>
2017-08-29 11:20:39 +1200

    Testing commit from personal computer

---
+++ src/main/java/seng202/team7/MainWindow.java
@@ -0,0 +1,16 @@
+package seng202.team7;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainWindow extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        //Testing a push from personal computer
+    }
+}
