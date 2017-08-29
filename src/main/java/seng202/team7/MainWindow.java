d55d4b69e67d4f9d237e63e72326d07499340f51
Connor <cmc274@uclive.ac.nz>
2017-08-29 15:04:30 +1200

    Created MainWindow class and added basic title and grid

--- src/main/java/seng202/team7/MainWindow.java
+++ src/main/java/seng202/team7/MainWindow.java
@@ -1,6 +1,9 @@
 package seng202.team7;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class MainWindow extends Application {
@@ -11,6 +14,18 @@ public class MainWindow extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        //Testing a push from personal computer
+        primaryStage.setTitle("Meraki Bikes");
+
+        // Create the gridpane for the overall main panel
+        GridPane mainGridPane = new GridPane();
+        mainGridPane.setAlignment(Pos.CENTER);
+
+        // For debugging purposes only
+        mainGridPane.setGridLinesVisible(true);
+
+        Scene mainScene = new Scene(mainGridPane, 1000, 600);
+        primaryStage.setScene(mainScene);
+
+        primaryStage.show();
     }
 }
