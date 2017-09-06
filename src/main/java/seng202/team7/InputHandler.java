7133c5a6f9f4aaadddff9a0a961ac3419f6f07a7
Lachlan Brewster <lbr63@uclive.ac.nz>
2017-09-06 12:42:31 +1200

    Input handler now checks validity of data, actual individual methods not filled in yet, added handling for items that werent in the constructor or actual trip classes, e.g gender, birthYear

--- src/main/java/seng202/team7/InputHandler.java
+++ src/main/java/seng202/team7/InputHandler.java
@@ -4,36 +4,42 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-//import org.apache.commons.csv.CSVFormat;
-//import org.apache.commons.csv.CSVParser;
-//import org.apache.commons.csv.CSVRecord;
+import java.util.Date;
+
 
 public class InputHandler {
 
 
     /*
-
-    USED FOR TESTING ACTUAL FUNCTIONALITY,
-    HAVE TO MAKE INPUT HANDLER STATIC THOUGH LOL
-
+    USED FOR TESTING ACTUAL FUNCTIONALITY, put print statements on each test object created,
+    e.g printThis(tripDataTest.getCity())
+    */
     public static void main(String[] args) {
 
 
         try {
-            //loadCSV("wifi_data.csv", "wifi");
-            loadCSV("retailer_data.csv", "retailer");
-            //loadCSV("trip_data.csv", "trip");
+
+            InputHandler toTest = new InputHandler();
+
+            toTest.loadCSV("retailer_data.csv", "retailer");
+            toTest.loadCSV("trip_data.csv", "trip");
+            toTest.loadCSV("wifi_data.csv", "wifi");
         } catch (IOException e) {
             e.printStackTrace();
         }
 
     }
-*/
 
 
-    /*
-    Method to parse a given csv file using the apache package ORRR using default IO package
-    buffered reader and split line () etc
+
+    /**
+     *     Method to parse a given csv file using the default IO package and buffered reader using split line () etc
+     *     Returns an array of objects of type Trip, Wifi, or Retailer
+     * @param file
+     * @param dataType
+     * @return data
+     * @throws IOException
+     * @throws NumberFormatException
      */
     public ArrayList<Data> loadCSV(String file, String dataType) throws IOException, NumberFormatException
     {
@@ -41,21 +47,7 @@ public class InputHandler {
         ArrayList<Data> data = new ArrayList<Data>();  //will add multiple objects, so need an array
         Data dataToAdd = null;   //individual data packets to add to DB, each are one object
 
-        /*
-        Using apache parser, might just use built in stuff as have more control seems like
-
-        CSVParser parser = new CSVParser(new FileReader("blahblahblah.csv"), CSVFormat.DEFAULT.withHeader());
-
-        for (CSVRecord record : parser) {
-            System.out.printf("%s\t%s\t%s\n", record.get("NAME"),
-                    record.get("CAPITAL"), record.get("CURRENCY"));
-        }
-        parser.close();
-        */
-
-
-        //file in format "blahblahblah.csv"
-        BufferedReader reader = new BufferedReader(new FileReader(file));
+        BufferedReader reader = new BufferedReader(new FileReader(file));   //file in format "blahblahblah.csv"
         String line = reader.readLine(); // Reading header, Ignoring/getting rid of it if there is one
 
 
@@ -72,7 +64,7 @@ public class InputHandler {
                         String provider = fields[4];
                         String location = fields[6];
 
-                        String dataGroup = "0";
+                        String dataGroup = "default";
                         double longitude = Double.parseDouble(fields[8]);
                         double latitude = Double.parseDouble(fields[7]);
 
@@ -87,7 +79,7 @@ public class InputHandler {
                         String SSID;
 
                         try {
-                            String test = fields[29];
+                            String test = fields[29];    //if it sets off a out of array error then add the fields together
 
                             remarks = fields[12] + "," + fields[13];
                             city = fields[14];
@@ -99,8 +91,11 @@ public class InputHandler {
                             SSID = fields[14];
                         }
 
+                        Wifi wifiDataTest = new Wifi(burough, type, provider, location, city, SSID, remarks, dataGroup, longitude, latitude); //temp test object
+                        if (checkValidity(wifiDataTest)) {
+                            dataToAdd = new Wifi(burough, type, provider, location, city, SSID, remarks, dataGroup, longitude, latitude);   //create actual 'Data' object
+                        }
 
-                        dataToAdd = new Wifi(burough, type, provider, location, city, SSID, remarks, dataGroup, longitude, latitude);   //create actual object
                         break;
 
 
@@ -108,66 +103,84 @@ public class InputHandler {
                         String name = fields[0];
                         city = fields[3];
                         String pAddress = fields[1];
-                        String sAddress = "0";     //eh?
+                        String sAddress = fields[2];
                         String state = fields[4];
                         int zipCode = Integer.parseInt(fields[5]);
                         type = fields[7];
+                        String typeID = fields[8];
 
-                        String typeID = "0";         //doesn't have it yo
-                        dataGroup = "0";
+                        dataGroup = "default";
 
-                        dataToAdd = new Retailer(name, city, pAddress, sAddress, state, zipCode, type, typeID, dataGroup);   //create actual object
+                        Retailer retailerDataTest = new Retailer(name, city, pAddress, sAddress, state, zipCode, type, typeID, dataGroup);  //temp test object
+                        if (checkValidity(retailerDataTest)) {
+                            dataToAdd = new Retailer(name, city, pAddress, sAddress, state, zipCode, type, typeID, dataGroup);   //create actual 'Data' object
+                        }
+
                         break;
 
                     case "trip":
+                        int duration = Integer.parseInt(fields[0]);
+                        dataGroup = "default";
+                        String userType = fields[12];
+                        int bikeID = Integer.parseInt(fields[11]);
+                        String gender = fields[14];
+                        int birthYear = Integer.parseInt(fields[13]);
+                        String startDate = fields[1];
+                        String endDate = fields[2];
 
                         int startStationID = Integer.parseInt(fields[3]);
                         String startStationAddress = fields[4];
-                        String startStationDataGroup = "0";
+                        String startStationDataGroup = "default";
                         double startStationLat = Double.parseDouble(fields[5]);
                         double startStationLong = Double.parseDouble(fields[6]);
 
                         int endStationID = Integer.parseInt(fields[7]);
                         String endStationAddress = fields[8];
-                        String endStationDataGroup = "0";
+                        String endStationDataGroup = "default";
                         double endStationLat = Double.parseDouble(fields[9]);
                         double endStationLong = Double.parseDouble(fields[10]);
 
                         //create stations for creating a trip object
-                        Station startStation = new Station(startStationID, startStationAddress, startStationDataGroup, startStationLat, startStationLong);   //Station
-                        Station endStation = new Station(endStationID, endStationAddress, endStationDataGroup, endStationLat, endStationLong);   //Station
+                        //testing station validity, else ditch this piece of data
+                        Station startStation = new Station(startStationID, startStationAddress, startStationDataGroup, startStationLat, startStationLong);
+                        if (!checkValidity(startStation)) {
+                            break;
+                        }
 
-                        int duration = Integer.parseInt(fields[0]);
+                        //testing station validity, else ditch this piece of data
+                        Station endStation = new Station(endStationID, endStationAddress, endStationDataGroup, endStationLat, endStationLong);
+                        if (!checkValidity(endStation)) {
+                            break;
+                        }
 
 
-                        //WHERE ARE THESE RECORDED?
-                        //CAN EASY ADD IT IN HERE, JUST DON'T KNOW IF IM MEANT TO
-                        /*
-                        String userType = fields[12];
-                        String bikeID = fields[11];     //int
-                        String gender = fields[14];    //int
-                        String birthYear = fields[13]; //int
-                        dataGroup = "0";
-                        */
 
-                        dataToAdd = new Trip(startStation, endStation, duration);  //create actual object
+                        Trip tripDataTest = new Trip(startStation, endStation, duration, startDate, endDate, userType, birthYear, gender, dataGroup); //temp test object
+                        if (checkValidity(tripDataTest)) {
+                            dataToAdd = new Trip(startStation, endStation, duration, startDate, endDate, userType, birthYear, gender, dataGroup);  //create actual 'Data' object
+                        }
+
                         break;
 
 
                     }
 
-                } catch (NumberFormatException| ArrayIndexOutOfBoundsException e ){
+                } catch (NumberFormatException | ArrayIndexOutOfBoundsException e ){
                 //e.printStackTrace();
                 //System.out.println("Wrong type of data");
 
             }
 
-            data.add(dataToAdd); //add object into array to be returned
+
+            if (dataToAdd != null) {         //if object wasn't valid then don't add it
+                data.add(dataToAdd);         //add object into array to be returned
+
+            }
 
         }
 
-        reader.close();  //don't need it anymore
-        return data;  //return appropriate array for use
+        reader.close();    //don't need it anymore
+        return data;       //return appropriate array of objects for use
 
     }
 
@@ -176,26 +189,183 @@ public class InputHandler {
 
 
 
-//don't need quite yet as if the data is invalid it'll just set off an error as i try parse it
+    /**
+     * Tests an inputted Data objects data individually to see if it is valid, returns true if valid
+     * @param dataToTest
+     * @return
+     */
+    private Boolean checkValidity(Data dataToTest)
+    {
+        return true;
+    }
 
+    /**
+     * Tests an inputted retailer objects data individually to see if it is valid, returns true if valid
+     * @param retailer
+     * @return validRetailer
+     */
     private Boolean checkValidity(Retailer retailer)
     {
-        return true;
+
+        boolean validRetailer = true;
+
+        /*
+        if (retailer.getCity() != ) {
+            validRetailer = false;
+        }
+        else if (retailer.getName() != ) {
+            validRetailer = false;
+        }
+        else if (retailer.getPAddress() != ) {
+            validRetailer = false;
+        }
+        else if (retailer.getSAddress() != ) {
+            validRetailer = false;
+        }
+        else if (retailer.getState() != ) {
+            validRetailer = false;
+        }
+        else if (retailer.getZipCode() != ) {           //int
+            validRetailer = false;
+        }
+        else if (retailer.getType() != ) {
+            validRetailer = false;
+        }
+        else if (retailer.getTypeID() != ) {
+            validRetailer = false;
+        }
+        else if (retailer.getDataGroup() != ) {
+            validRetailer = false;
+        }*/
+
+
+
+
+        return validRetailer;
     }
 
+    /**
+     * Tests an inputted trip objects data individually to see if it is valid, returns true if valid
+     * @param trip
+     * @return validRetailer
+     */
     private Boolean checkValidity(Trip trip)
     {
-        return true;
+        boolean validRetailer = true;
+
+        /*
+        if (trip.getDuration() != ) {          //int
+            validRetailer = false;
+        }
+        else if (trip.getStart() != ) {         //station
+            validRetailer = false;
+        }
+        else if (trip.getEnd() != ) {           //station
+            validRetailer = false;
+        }
+
+        else if (trip.getDataGroup() != ) {
+            validRetailer = false;
+        }
+
+        else if (trip.getGender() != ) {                 //int  //not in constructor
+            validRetailer = false;
+        }
+
+        else if (trip.getBirthYear() != ) {              //int   //not in constructor
+            validRetailer = false;
+        }
+        else if (trip.getBikeID() != ) {               //not in constructor
+            validRetailer = false;
+        }
+
+        else if (trip.getUserType() != ) {            //not in constructor
+            validRetailer = false;
+        }*/
+
+
+
+        return validRetailer;
+
     }
 
+    /**
+     * Tests an inputted wifi objects data individually to see if it is valid, returns true if valid
+     * @param wifi
+     * @return validRetailer
+     */
     private Boolean checkValidity(Wifi wifi)
     {
-        return true;
+
+        boolean validRetailer = true;
+
+        /*
+        if (wifi.getBurough() != ) {
+            validRetailer = false;
+        }
+        else if (wifi.getType() != ) {
+            validRetailer = false;
+        }
+        else if (wifi.getProvider() != ) {
+            validRetailer = false;
+        }
+        else if (wifi.getLocation() != ) {
+            validRetailer = false;
+        }
+
+        else if (wifi.getDataGroup() != ) {
+            validRetailer = false;
+        }
+        else if (wifi.getLatitude() != ) {         //double
+            validRetailer = false;
+        }
+        else if (wifi.getLongitude() != ) {        //double
+            validRetailer = false;
+        }
+        else if (wifi.getRemarks() != ) {
+            validRetailer = false;
+        }
+        else if (wifi.getCity() != ) {
+            validRetailer = false;
+        }
+        else if (wifi.getSSID() != ) {
+            validRetailer = false;
+        }*/
+
+
+
+        return validRetailer;
+
     }
 
+    /**
+     * Tests an inputted station objects data individually to see if it is valid, returns true if valid
+     * @param station
+     * @return validRetailer
+     */
     private Boolean checkValidity(Station station)
     {
-        return true;
+
+        boolean validRetailer = true;
+        /*
+        if (station.getId() != ) {                    //int
+            validRetailer = false;
+        }
+        else if (station.getAddress() != ) {
+            validRetailer = false;
+        }
+        else if (station.getDataGroup() != ) {
+            validRetailer = false;
+        }
+        else if (station.getLatitude() != ) {             //double
+            validRetailer = false;
+        }
+        else if (station.getLongitude() != ) {            //double
+            validRetailer = false;
+        }*/
+
+
+        return validRetailer;
     }
 
 
