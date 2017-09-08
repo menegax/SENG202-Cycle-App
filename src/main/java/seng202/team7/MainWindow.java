f0567d18dca0a81c21415ee887c0f2d4436ddb2c
Connor <cmc274@uclive.ac.nz>
2017-09-09 10:50:12 +1200

    Added tree structure navigation bar to MainWindow class

--- src/main/java/seng202/team7/MainWindow.java
+++ src/main/java/seng202/team7/MainWindow.java
@@ -1,9 +1,13 @@
 package seng202.team7;
 
 import javafx.application.Application;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class MainWindow extends Application {
@@ -12,20 +16,72 @@ public class MainWindow extends Application {
         launch(args);
     }
 
+    GridPane mainPane = new GridPane();
+
     @Override
     public void start(Stage primaryStage) {
+        // Creates the main grid pane and add the navigation pane to it
+        TreeView<String> navigationTree = createNavigationTree();
+        StackPane navigationPane = new StackPane();
+        navigationPane.getChildren().add(navigationTree);
+        mainPane.add(navigationPane, 0, 0);
+
+        // Creates the main scene pane
+        Scene mainScene = new Scene(mainPane, 500, 500);
+
+        // Main stage
         primaryStage.setTitle("Meraki Bikes");
-
-        // Create the gridpane for the overall main panel
-        GridPane mainGridPane = new GridPane();
-        mainGridPane.setAlignment(Pos.CENTER);
-
-        // For debugging purposes only
-        mainGridPane.setGridLinesVisible(true);
-
-        Scene mainScene = new Scene(mainGridPane, 1000, 600);
         primaryStage.setScene(mainScene);
-
         primaryStage.show();
+    }
+
+
+    // Creates branches for a tree view
+    private TreeItem<String> makeBranch(String title, TreeItem<String> parent) {
+        TreeItem<String> item = new TreeItem<>(title);
+        //item.setExpanded(true);
+        parent.getChildren().add(item);
+        return item;
+    }
+
+    // Creates the navigation bar
+    private TreeView<String> createNavigationTree() {
+        TreeItem<String> root, home, routePlanning, analytics, dataViewer, dataEntry, help;
+
+        // Root
+        root = new TreeItem();
+        root.setExpanded(true);
+
+        // Home branch
+        home = makeBranch("Home", root);
+
+        // Route planning branch
+        routePlanning = makeBranch("Route Planning", root);
+
+        // Analytics branch
+        analytics = makeBranch("Analytics", root);
+
+        // Data Viewer branch
+        dataViewer = makeBranch("Raw Data Viewer", root);
+        makeBranch("Retailer", dataViewer);
+        makeBranch("Station", dataViewer);
+        makeBranch("Trip", dataViewer);
+        makeBranch("Wifi", dataViewer);
+
+        // Data Entry branch
+        dataEntry = makeBranch("Data Entry", root);
+        makeBranch("Retailer", dataEntry);
+        makeBranch("Station", dataEntry);
+        makeBranch("Trip", dataEntry);
+        makeBranch("Wifi", dataEntry);
+
+        // Help branch
+        help = makeBranch("Help", root);
+
+        // Create tree
+        TreeView<String> navigationTree = new TreeView<>(root);
+        navigationTree.setShowRoot(false);
+
+        return navigationTree;
     }
 }
