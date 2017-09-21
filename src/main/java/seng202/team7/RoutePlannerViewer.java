83c8753bebc62833c39c7e81ea2035bf4926f969
jam357 <jam357@uclive.ac.nz>
2017-09-21 13:19:54 +1200

    RoutePlannerViewer Basic functionality in GUI

---
+++ src/main/java/seng202/team7/RoutePlannerViewer.java
@@ -0,0 +1,21 @@
+package seng202.team7;
+/**
+ * Created by jam357 on 19/09/17.
+ */
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import java.io.IOException;
+
+public class RoutePlannerViewer extends AnchorPane {
+
+    public RoutePlannerViewer() {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("RoutePlannerViewer.fxml"));
+        fxmlLoader.setRoot(this);
+        try {
+            fxmlLoader.load();
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+}
