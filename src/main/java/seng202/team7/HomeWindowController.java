a23942b406f8504df80c469ae3ea94c25342cfd7
Connor <cmc274@uclive.ac.nz>
2017-09-24 21:44:41 +1300

    Got home page complete up to Deliverable 2 standard.

--- src/main/java/seng202/team7/HomeWindowController.java
+++ src/main/java/seng202/team7/HomeWindowController.java
@@ -3,6 +3,8 @@ package seng202.team7;
 import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,13 +13,17 @@ public class HomeWindowController implements Initializable{
 
     public ImageView backgroundImage;
     public AnchorPane mainHomeAnchorPane;
+    public Text headingText;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         // Makes it so the image re-sizes
         backgroundImage.fitWidthProperty().bind(mainHomeAnchorPane.widthProperty());
-        //backgroundImage.fitHeightProperty().bind(mainHomeAnchorPane.heightProperty());
+        mainHomeAnchorPane.widthProperty().addListener((v, oldValue, newValue) -> setFontSize());
     }
 
-    
+    private void setFontSize(){
+        double size = mainHomeAnchorPane.getWidth() / 25;
+        headingText.setFont(Font.font("Tahoma", size));
+    }
 }
