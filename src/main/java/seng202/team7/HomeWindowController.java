1b190e88658ce2234c0929f4af535f510e225bff
Connor <cmc274@uclive.ac.nz>
2017-09-24 21:11:12 +1300

    Got an image working for the home page. Currently only scales one way.

--- src/main/java/seng202/team7/HomeWindowController.java
+++ src/main/java/seng202/team7/HomeWindowController.java
@@ -1,4 +1,23 @@
 package seng202.team7;
 
-public class HomeWindowController {
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomeWindowController implements Initializable{
+
+    public ImageView backgroundImage;
+    public AnchorPane mainHomeAnchorPane;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // Makes it so the image re-sizes
+        backgroundImage.fitWidthProperty().bind(mainHomeAnchorPane.widthProperty());
+        //backgroundImage.fitHeightProperty().bind(mainHomeAnchorPane.heightProperty());
+    }
+
+    
 }
