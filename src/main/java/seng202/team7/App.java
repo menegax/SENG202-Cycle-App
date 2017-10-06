31d420772edac8f1db3d52b250ef2346d31cbaa4
mgf29 <mgf29@uclive.ac.nz>
2017-10-06 15:07:25 +1300

    Retailer now display in exact locations rather than a guess based on zipcode and city

--- src/main/java/seng202/team7/App.java
+++ src/main/java/seng202/team7/App.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.sql.Connection;
 import java.util.ArrayList;
 
+
 /**
  * Hello world!
  *
@@ -21,6 +22,17 @@ public class App
         DatabaseRetriever databaseRetriever = new DatabaseRetriever();
 
         DatabaseHandler.getOnlineDatabase();
+        Retailer retailer = new Retailer("bob's burgers", "New York", "3 New York Plaza", "" ,"NY", 10004, "F-Coffeehouse", "Casual Eating & Takeout", "retailer");
+        String new_address = retailer.getPAddress() +  ", " + retailer.getCity() + ", " + retailer.getState();
+        new_address = new_address.replaceAll(" ", "+");
+        System.out.println(new_address);
+        System.out.println(retailer.getLatitude());
+        System.out.println(retailer.getLongitude());
+
+        Retailer retailers = new Retailer("bob's burgers", "New York", "57 Murray Street", "" ,"NY", 10007, "F-Coffeehouse", "Casual Eating & Takeout", "retailer");
+        System.out.println(retailers.getLatitude());
+        System.out.println(retailers.getLongitude());
+
 
 
 //        ArrayList<Trip> tempT= new ArrayList<Trip>();
